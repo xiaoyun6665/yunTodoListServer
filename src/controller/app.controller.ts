3b8946cb346384f60e05b6faa4cba09292c1ca2c
云孜微殇 <1963945679@qq.com>
2022-05-02 23:05:48 +0800

    feat: getItemByDay

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -4,10 +4,16 @@ import { AppService } from '../services/app.service';
 
 @Controller('/todoitem')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/getOneTodoItem')
   getOneTodoItem(@Query() params): Promise<Todoitem> {
     return this.appService.getOneTodoItem(params.id);
+  }
+
+  @Get('/getOneTodoItemByDay')
+  getOneTodoItemByDay(@Query() params): Promise<Todoitem[]> {
+    return this.appService.getOneTodoItemByDay(
+      params.year, params.month, params.day, params.pageSize, params.pages);
   }
 }
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -6,11 +6,28 @@ import { Todoitem } from '../model/todoitem.entity'
 @Injectable()
 export class AppService {
   constructor(
-    @InjectRepository(Todoitem) 
+    @InjectRepository(Todoitem)
     private todoItemRepository: Repository<Todoitem>
-  ){}
+  ) { }
 
   getOneTodoItem(id: number): Promise<Todoitem> {
     return this.todoItemRepository.findOne(id);
+  }
+
+  getOneTodoItemByDay(
+    year: number, month: number, day: number,
+    pageSize: number, pages: number
+  ): Promise<Todoitem[]> {
+    return this.todoItemRepository.find({
+      where: {
+        createDay: day, createMonth: month,
+        createYear: year
+      },
+      order: {
+        id: 'ASC'
+      },
+      skip: pages * pageSize,
+      take: pageSize
+    });
   }
 }
